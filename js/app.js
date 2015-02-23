ce041cb302b523823d60b5d03337ea353bfd267a
Max Dumas <maltor124@gmail.com>
2015-02-23 15:01:57 -0500

    Projects are now data-based.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function() {
+	var source = $('#project-item-template').html();
+
+	var template = Handlebars.compile(source);
+
+	$.get('/data/projects.json', function(data) {
+		data.projects.forEach(function(project) {
+			var html = template(project);	
+			$('#project-list').append(html);
+		});
+	});
+})();
