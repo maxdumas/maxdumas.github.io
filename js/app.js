e8e9764a96634c189bf874060d66a773e5f85b72
Max Dumas <maltor124@gmail.com>
2015-02-23 21:27:25 -0500

    Added thumbnails. Many style changes.

--- js/app.js
+++ js/app.js
@@ -3,53 +3,6 @@
 
 	var template = Handlebars.compile(source);
 
-// 	var data = {
-// 		"projects": [
-// 		{
-// 			"title": "NYU: Poetic Landscape",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/poetic",
-// 			"description": "A soothing scene in Unity"
-// 		},
-// 		{
-// 			"title": "NYU: Treasure Game",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/treasure",
-// 			"description": "Deserts winds in 1918 obscure treasure untold"
-// 		},
-// 		{
-// 			"title": "NYU: Rube Goldberg Machine",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/goldberg",
-// 			"description": "A fantastic contraption for a moonlit night"
-// 		},
-// 		{
-// 			"title": "NYU: Rube Goldberg Machine",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/goldberg",
-// 			"description": "A fantastic contraption for a moonlit night"
-// 		},
-// 		{
-// 			"title": "NYU: Rube Goldberg Machine",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/goldberg",
-// 			"description": "A fantastic contraption for a moonlit night"
-// 		},
-// 		{
-// 			"title": "NYU: Rube Goldberg Machine",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/goldberg",
-// 			"description": "A fantastic contraption for a moonlit night"
-// 		},
-// 		{
-// 			"title": "NYU: Rube Goldberg Machine",
-// 			"image": "img/uap.png",
-// 			"url": "/nyu/goldberg",
-// 			"description": "A fantastic contraption for a moonlit night"
-// 		}
-// 	]
-// }
-
 	$.get('/data/projects.json', function(data) {
 		var i = 0;
 		var showProject = function() {
