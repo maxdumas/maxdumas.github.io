1cf1e2f73380e046f10a768736d60e6add9909fb
Max Dumas <maltor124@gmail.com>
2015-03-01 20:25:26 -0500

    Add masonry to align projects better.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
+'use strict';
+
 (function() {
 	var source = $('#project-item-template').html();
 
 	var template = Handlebars.compile(source);
 
 	$.get('/data/projects.json', function(data) {
+		var projects = $('#project-list');
+
 		data.projects.forEach(function(project) {
 			var html = template(project);	
-			$('#project-list').append(html);
+			projects.append(html);
+		});
+
+		imagesLoaded(projects, function() {
+			projects.masonry({
+				itemSelect: '.project'
+			});
 		});
 	});
 })();
